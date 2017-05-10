ada0071114f5fb075df38ffa34c487297a6d44fb
Kachiie <Nwan@Kachis-MacBook-Pro.local>
2017-05-10 08:42:58 -0500

    New commit

--- jsCopy/navSwitch.js
+++ jsCopy/navSwitch.js
@@ -5,8 +5,8 @@ $(function(){
             $Nav.removeClass('w3-hide').addClass('w3-show');
         } else if ($Nav.hasClass('w3-show')){ 
             $Nav.removeClass('w3-show').addClass('w3-hide');
+            $Nav.on('click', function(){$Nav.removeClass('w3-show').addClass('w3-hide');});
         }
-
     }); 
-
 }); 
+
